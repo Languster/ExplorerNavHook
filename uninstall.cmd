c649b581c06702a6667aa9c6ab2e65c6448944d0
Languster <arm.fight365@gmail.com>
2026-04-06 22:23:43 +0300

    Update uninstall.cmd

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,12 +1,17 @@
 @echo off
-setlocal
+setlocal EnableExtensions
 
 taskkill /im ExplorerNavHook.exe /f >nul 2>&1
 
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" ^
- /v ExplorerNavHook ^
- /f >nul 2>&1
+rem Удаляем задачу Планировщика
+schtasks /Delete /TN "ExplorerNavHook" /F >nul 2>&1
+
+rem На всякий случай убираем и старую запись автозагрузки через Run
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v ExplorerNavHook /f >nul 2>&1
 
 echo Unregistered and stopped:
 echo ExplorerNavHook.exe
+echo.
+echo Scheduled Task removed:
+echo ExplorerNavHook
 pause
